102c65a882ddbf500e75e7aa498b94b09e1a9e30
day Lee <dayeong.lee@uwaterloo.ca>
2018-07-19 22:56:19 -0400

    Context manager

--- cakechat/api/v1/server.py
+++ cakechat/api/v1/server.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 
 from cakechat.api.response import get_response
+from cakechat.api.context_man import *
 from cakechat.api.utils import get_api_error_response, parse_dataset_param
 from cakechat.config import EMOTIONS_TYPES, DEFAULT_CONDITION
 from cakechat.utils.logger import get_logger
@@ -9,7 +10,7 @@ from cakechat.utils.profile import timer
 _logger = get_logger(__name__)
 
 app = Flask(__name__)
-
+contextMan = ContextManager()
 
 @app.route('/felix_api/v1/actions/get_response', methods=['POST'])
 @timer
@@ -26,14 +27,16 @@ def get_model_response():
     #                                   (emotion, list(EMOTIONS_TYPES)), 400, _logger)
 
     # response = get_response(dialog_context, emotion)
-    dialog = [sentence]
+    dialog = contextMan.get_contexts(sentence)
     emotion = "neutral"
     response = get_response(dialog, emotion)
 
     if not response:
         _logger.error('No response for context: %s; emotion "%s"' % (dialog, emotion))
+        # TODO: shouldn't you return some other response code?
         return jsonify({}), 200
 
+    contextMan.update_contexts(response)
     _logger.info('Given response: "%s" for context: %s; emotion "%s"' % (response, dialog, emotion))
 
     return jsonify(fulfillmentText=response), 200
